e97a49eba658babbf9948af0004c36c0e5b9be5e
askarthikey <179118645+askarthikey@users.noreply.github.com>
2025-05-03 23:27:48 +0530

    backend-4

--- server/server.js
+++ server/server.js
@@ -1,37 +1,31 @@
 const express = require("express");
 const { MongoClient, MongoError } = require("mongodb");
-const dotenv = require("dotenv");
+const dotenv = require("dotenv").config();
 const userApp = require("./APIs/userApi");
 const workshopApp = require("./APIs/workshopApi");
 const cors = require('cors');
-
-dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
-const connectDB = async () => {
-  try {
-    const client = await MongoClient.connect(process.env.DB_URL, {
-      tls: true,
-      tlsAllowInvalidCertificates: false,
-      tlsAllowInvalidHostnames: false,
-    });
+MongoClient.connect(process.env.DB_URL)
+  .then(client => {
+    // Get database object
     const db = client.db("nexuZ");
+    
+    // Get collection objects
     const usersCollection = db.collection("usersCollection");
     const workshopCollection = db.collection("workshopCollection");
+    
+    // Share collection objects with express app
     app.set("usersCollection", usersCollection);
     app.set("workshopCollection", workshopCollection);
+    
+    // Confirm db connection status
     console.log("DB connection successful!!");
-  } catch (err) {
-    console.log("Error in connection of database", err.message);
-  }
-};
-
-// Initialize DB connection
-connectDB();
+  })
+  .catch(err => console.log("Error in connection of database", err.message));
 
 app.use("/userApi", userApp);
 app.use("/workshopApi", workshopApp);
